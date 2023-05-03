924b3ffc5067a67a7b1618de3b72b418d802bae9
Gulzarali09 <121237439+Gulzarali09@users.noreply.github.com>
2023-05-03 21:55:06 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCpYJMXo-bzHWq24Yx1OHbNM7yH_uQ-UFg",
+    authDomain: "class100-29dfb.firebaseapp.com",
+    databaseURL: "https://class100-29dfb-default-rtdb.firebaseio.com/",
+    projectId: "class100-29dfb",
+    storageBucket: "class100-29dfb.appspot.com",
+    messagingSenderId: "435840654788",
+    appId: "1:435840654788:web:a03eba6cde266eb46ed891"
+    
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
